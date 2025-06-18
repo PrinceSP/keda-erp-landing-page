a251ad65d05c661defcb410809ed0ba669c00369
PrinceSP <princepasombaran@gmail.com>
2025-06-18 22:37:06 +0700

    feat: export new components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,7 @@ import Navbar from "./navbar"
 import Footer from "./footer"
 import Hero from "./hero"
 import Partner from "./partner"
+import Solutions from "./solutions"
+import Services from "./services"
 
-export { Navbar, Footer, Hero, Partner }
+export { Navbar, Footer, Hero, Partner, Solutions, Services }
