804bdde835cb2029943b89af15dafd428d36153b
PrinceSP <princepasombaran@gmail.com>
2025-06-19 06:03:51 +0700

    feat: slicing footer components

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,56 @@
+import { BsTwitterX } from "react-icons/bs"
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa6"
+
+const Footer = () => {
+  const sitemap = [
+    {
+      heading: "Company",
+      menus: ["About us", "Contact us", "Careers", "Blogs"]
+    },
+    {
+      heading: "Product",
+      menus: ["Features", "Pricing", "News", "Help desk", "Support"]
+    },
+    {
+      heading: "Services",
+      menus: ["POS System", "Sales Analysis", "SEO for Business", "Taxation"]
+    },
+    {
+      heading: "Legal",
+      menus: ["Privacy Policy", "Terms & Conditions", "Return Policy"]
+    },
+    {
+      heading: "Contact us",
+      menus: ["support@kedaerp.io", "+123-456-7895-2121"]
+    }
+  ]
+  return (
+    <footer className="w-screen flex flex-col items-center justify-between pt-50 px-40 bg-[#161C2D]">
+      <section className="w-4/5 h-auto flex items-start justify-between gap-[40px]">
+        {sitemap.map(item =>
+          <div className="flex flex-col items-start gap-10">
+            <p className="text-[0.937em] text-left text-white/65 font-inter-regular">{item.heading}</p>
+            <ul className="flex flex-col items-start gap-4">
+              {item.menus.map(menuList =>
+                <li>
+                  <a href={`#${menuList.split(" ").join("").toLowerCase()}`} className="text-white text-[1.062em] text-left font-inter-regular">{menuList}</a>
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+      </section>
+      <section className="w-4/5 h-30 mt-24 py-6 border-t-1 border-[#292F41] flex items-center justify-between">
+        <p className="text-white/65 text-[0.937em] font-inter-regular">© 2025 Copyright, All Right Reserved, Made by <span className="font-inter-bold">PrinceSiachin</span>.</p>
+        <div className="flex items-center justify-between gap-4">
+          <FaInstagram color="#fff" />
+          <BsTwitterX color="#fff" />
+          <FaFacebookF color="#fff" />
+          <FaLinkedinIn color="#fff" />
+        </div>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
