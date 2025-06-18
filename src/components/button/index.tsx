df458f49d8bec3116b24ba3997f9380bb5ea6599
PrinceSP <princepasombaran@gmail.com>
2025-06-19 05:25:04 +0700

    ref(style): change text fonts and size

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,19 +1,12 @@
-import React, { forwardRef } from "react"
+import { forwardRef } from "react"
+import type { BtnProps } from "../../types"
 
-interface BtnProps {
-  id?: string | undefined;
-  title: string;
-  leftIcon?: React.ReactNode;
-  rightIcon?: React.ReactNode;
-  containerClass?: string;
-}
-
-const Button = forwardRef<HTMLButtonElement, BtnProps>(({ id, title, leftIcon, rightIcon, containerClass }, ref) => {
+const Button = forwardRef<HTMLButtonElement, BtnProps>(({ onClick, id, title, leftIcon, rightIcon, containerClass, color = "text-[#fff]", radius = "rounded-2xl", padding = "px-7 py-5", size, family }, ref) => {
   return (
-    <button ref={ref} id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-2xl px-7 py-5 text-black ${containerClass}`}>
+    <button ref={ref} id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden ${padding} ${radius} ${containerClass}`} onClick={onClick} type="button">
       {leftIcon}
-      <span className="relative overflow-hidden uppercase incline-flex font-inter-semibold text-base text-[#fff]">
-        <p>{title}</p>
+      <span className={`relative overflow-hidden incline-flex text-base ${color}`}>
+        <p className={`${size} ${family}`}>{title}</p>
       </span>
       {rightIcon}
     </button>
