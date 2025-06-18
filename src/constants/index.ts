3629405a66d716c00e7e4ceb960b712da2b2c9f2
PrinceSP <princepasombaran@gmail.com>
2025-06-19 05:16:52 +0700

    feat: add constant variable datas

---
+++ src/constants/index.ts
@@ -0,0 +1,116 @@
+export const partnerLogo = [
+  "/illustrations/airbnb.svg",
+  "/illustrations/hubspot.svg",
+  "/illustrations/google.svg",
+  "/illustrations/microsoft.svg",
+  "/illustrations/airbnb.svg",
+  "/illustrations/walmart.svg",
+  "/illustrations/fedex.svg"
+]
+
+export const pricingDatas = [
+  {
+    bg: "FFF8F6",
+    color: "F5715C",
+    img: "illustrations/pricings/basic.svg",
+    tier: "Basic",
+    price: "At $0",
+    desc: "for You",
+    features: [
+      "Manage company profile",
+      "Add - manage multiple storage / locations",
+      "Product master list",
+      "Stock in/out per store",
+      "Transaction log",
+      "Profit Tracking"
+    ]
+  },
+  {
+    bg: "F7F9FF",
+    color: "458DF6",
+    img: "illustrations/pricings/business.svg",
+    tier: "Business:",
+    price: "At $50",
+    desc: "Limited Offer",
+    features: [
+      "User and Access Management",
+      "Stock Management System (In/Out)",
+      "Sales Order and Transaction Processing",
+      "Returns Management",
+      "Cash Receipt Management",
+      "Purchase Management",
+      "Sales Analysis Via Chart",
+      "Delivery & Shipping",
+      "Profit Tracking",
+      "24/7  Support with our team"
+    ]
+  },
+  {
+    bg: "F1FBF7",
+    color: "17BE87",
+    img: "illustrations/pricings/entrepreneur.svg",
+    tier: "Entrepeneur:",
+    price: "Get Best Quote",
+    desc: "On Demand",
+    features: [
+      "Material Requisition & Return Management",
+      "Sales Order Management",
+      "Stock Management System (In/Out)",
+      "Finance and Accounting + Quality Control",
+      "Payment and Financial Management",
+      "Sales Analysis Via Chart",
+      "AI Income Prediction",
+      "Data Export (Excel & CSV)",
+      "24/7  Support With Our Team",
+      "Offline Syncing"
+    ]
+  }
+]
+
+export const servicesDatas = [
+  {
+    title: "Online and Offline Sync",
+    desc: "Offline orders sync automatically when switching to the online mode of the KeDa ERP system.",
+    icon: "illustrations/services/convert-3d-cube.svg"
+  },
+  {
+    title: "Supplier Management",
+    desc: "Reduce 60% of time by tracking, collaborating, qualifying, transacting, and monitoring suppliers.",
+    icon: "illustrations/services/3d-rotate.svg"
+  },
+  {
+    title: "Multi Location",
+    desc: "Manage and track multiple stores across locations with one software, enabling easy expansion",
+    icon: "illustrations/services/location-add.svg"
+  },
+  {
+    title: "Multi Users ",
+    desc: "The unlimited license authorizes the use of the KeDa concurrently by all users within an organization and has no limit on end-user installations.",
+    icon: "illustrations/services/profile-2user.svg"
+  },
+  {
+    title: "Integration",
+    desc: "KeDa offers a wide range of integration capabilities that allow businesses to seamlessly connect with third-party applications and services.",
+    icon: "illustrations/services/format-circle.svg"
+  },
+  {
+    title: "Order Management System",
+    desc: "Enhance order processing with real-time tracking and centralized sales data, ensuring accuracy and efficiency.",
+    icon: "illustrations/services/box.svg"
+  },
+  {
+    title: "Warehouse Management System",
+    desc: "Optimize inventory tracking and warehouse operations to maintain stock levels and reduce overhead costs.",
+    icon: "illustrations/services/house.svg"
+  },
+  {
+    title: "Integrated Accounting Operations",
+    desc: "Simplify finances with automated transactions and real-time insights, making financial management hassle-free.",
+    icon: "illustrations/services/driver-refresh.svg"
+  },
+  {
+    title: "HR & Payroll Management",
+    desc: "Efficiently manage employee data and payroll with integrated tools, ensuring compliance and accuracy.",
+    icon: "illustrations/services/user-edit.svg"
+  },
+]
