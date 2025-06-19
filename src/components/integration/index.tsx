25fd2011e78a94fd5a7bdc97acc5125ab36bf1a2
PrinceSP <princepasombaran@gmail.com>
2025-06-19 18:40:33 +0700

    style(fix): fixing responsive layout

--- src/components/integration/index.tsx
+++ src/components/integration/index.tsx
@@ -1,13 +1,13 @@
 const Integration = () => {
   return (
-    <div className='w-screen flex items-center justify-center py-20 px-28'>
-      <div className='w-5/6 flex items-center justify-between'>
-        <div className="w-[45%]">
-          <h1 className="text-wrap text-[#111827] text-5xl font-inter-bold">Easy integrations with 170+ tools</h1>
-          <p className="text-wrap text-[#111827] text-lg font-inter-regular">Easy to connect with your favorite tools that you use daily and keep things on track.</p>
+    <div className='w-screen flex items-center justify-center py-20 px-10 md:px-28'>
+      <div className='w-full md:w-5/6 flex flex-col md:flex-row items-center justify-between'>
+        <div className="w-full md:w-[45%]">
+          <h1 className="text-wrap text-[#111827] text-4xl md:text-5xl font-inter-bold">Easy integrations with 170+ tools</h1>
+          <p className="text-wrap text-[#111827] text-base md:text-lg font-inter-regular">Easy to connect with your favorite tools that you use daily and keep things on track.</p>
         </div>
-        <div className="relative pt-10 w-2/4 overflow-hidden min-h-[130px] sm:min-h-[170px] lg:min-h-[280px]">
-          <div className="absolute top-0 left-0 w-full h-[130px] sm:h-[170px] lg:h-[280px] bg-[#EDE9FE] rounded-b-full z-0" />
+        <div className="relative pt-10 w-full md:w-2/4 overflow-hidden min-h-[130px] md:min-h-[170px] lg:min-h-[280px]">
+          <div className="absolute top-0 left-0 w-full h-[130px] md:h-[170px] lg:h-[280px] bg-[#EDE9FE] rounded-b-full z-0" />
           <img src="illustrations/integration.svg" alt="Integration tools CRM" className="relative z-10 mx-auto" />
         </div>
       </div>
