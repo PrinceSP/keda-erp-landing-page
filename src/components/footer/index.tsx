e7f4874a046ae815fd67302c1d09adfdbfc5625f
PrinceSP <princepasombaran@gmail.com>
2025-06-19 18:37:05 +0700

    style(feat): styling footer content to be responsive

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -25,12 +25,12 @@ const Footer = () => {
     }
   ]
   return (
-    <footer className="w-screen flex flex-col items-center justify-between pt-50 px-40 bg-[#161C2D]">
-      <section className="w-4/5 h-auto flex items-start justify-between gap-[40px]">
+    <footer className="w-screen flex flex-col items-center justify-between pt-50 md:px-40 bg-[#161C2D]">
+      <section className="w-full md:w-4/5 h-auto flex flex-col md:flex-row items-start justify-between gap-[80px] md:gap-[40px]">
         {sitemap.map(item =>
-          <div className="flex flex-col items-start gap-10">
-            <p className="text-[0.937em] text-left text-white/65 font-inter-regular">{item.heading}</p>
-            <ul className="flex flex-col items-start gap-4">
+          <div className="w-full flex flex-col items-center md:items-start gap-10">
+            <p className="text-[0.937em] text-center md:text-left text-white/65 font-inter-regular">{item.heading}</p>
+            <ul className="flex flex-col items-center md:items-start gap-4">
               {item.menus.map(menuList =>
                 <li>
                   <a href={`#${menuList.split(" ").join("").toLowerCase()}`} className="text-white text-[1.062em] text-left font-inter-regular">{menuList}</a>
@@ -40,7 +40,7 @@ const Footer = () => {
           </div>
         )}
       </section>
-      <section className="w-4/5 h-30 mt-24 py-6 border-t-1 border-[#292F41] flex items-center justify-between">
+      <section className="w-4/5 h-30 mt-24 py-6 border-t-1 border-[#292F41] flex items-end md:items-center justify-between">
         <p className="text-white/65 text-[0.937em] font-inter-regular">© 2025 Copyright, All Right Reserved, Made by <span className="font-inter-bold">PrinceSiachin</span>.</p>
         <div className="flex items-center justify-between gap-4">
           <FaInstagram color="#fff" />
