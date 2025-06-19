93957cdc22c8438e007b63a9f7bfb8a576379005
PrinceSP <princepasombaran@gmail.com>
2025-06-19 18:37:53 +0700

    feat: new rounting to register and sign-in page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from "react-router";
+import { Register, SignIn } from './components/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index element={<App />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
