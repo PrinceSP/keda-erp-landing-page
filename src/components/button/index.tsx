e81480a3aa8ec498824870a33af1857f9f47a79a
PrinceSP <princepasombaran@gmail.com>
2025-06-19 19:10:41 +0700

    feat: add default value

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react"
 import type { BtnProps } from "../../types"
 
-const Button = forwardRef<HTMLButtonElement, BtnProps>(({ onClick, id, title, leftIcon, rightIcon, containerClass, color = "text-[#fff]", radius = "rounded-2xl", padding = "px-7 py-5", size, family }, ref) => {
+const Button = forwardRef<HTMLButtonElement, BtnProps>(({ onClick, id, title, leftIcon, rightIcon, containerClass="", color = "text-white", radius = "rounded-2xl", padding = "px-7 py-5", size, family }, ref) => {
   return (
     <button ref={ref} id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden ${padding} ${radius} ${containerClass}`} onClick={onClick} type="button">
       {leftIcon}
