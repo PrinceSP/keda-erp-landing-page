d3481f13670de3ad92a094050cca3fdd11df8aef
PrinceSP <princepasombaran@gmail.com>
2025-06-19 19:10:02 +0700

    feat: setup test configs so it can touch the dom

---
+++ src/setupTests.ts
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom'
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: any) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }),
+})
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   test: {
-    environment: 'jsdom'
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts'
   }
 })
